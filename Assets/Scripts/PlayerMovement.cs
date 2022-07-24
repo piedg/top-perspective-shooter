dd216e7d639bf1b8456ae37787182655a606b354
Pietro <pietro.digiovanni.dev@gmail.com>
2022-07-24 19:15:27 +0200

    added animation locomotion and script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
-
+    Animator animator;
     Vector3 moveInput;
     Rigidbody rb;
+    [SerializeField] Transform playerSpine;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -27,12 +29,26 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (rb.velocity.magnitude > 0)
+        {
+            // Player is moving
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+
+        }
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputZ = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Move X", inputX);
+        animator.SetFloat("Move Y", inputZ);
 
         moveInput.Set(inputX, rb.velocity.y, inputZ);
         moveInput.Normalize();
+    
 
         rb.velocity = moveInput * speed;
+
     }
 }
