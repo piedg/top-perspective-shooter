b6c9b5d6335795a275755a89b7aa697858be5af2
Pietro <pietro.digiovanni.dev@gmail.com>
2022-09-01 12:58:36 +0200

    no direction dodge fix

--- Assets/Scripts/StateMachines/Player/PlayerDodgeState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerDodgeState.cs
@@ -25,19 +25,20 @@ public class PlayerDodgeState : PlayerBaseState
     {
         remainingDodgeTime -= deltaTime;
 
-        if(dodgeDirection == Vector3.zero)
+        if (remainingDodgeTime <= 0f)
+        {
+            stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+            return;
+        }
+
+        if (dodgeDirection == Vector3.zero)
         {
             Move(stateMachine.transform.forward * stateMachine.DodgeForce, deltaTime);
+            return;
         }
         else
         {
             Move(dodgeDirection.normalized * stateMachine.DodgeForce, deltaTime);
-        }
-
-
-        if (remainingDodgeTime <= 0f)
-        {
-            stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
         }
 
         FaceMovementDirection(dodgeDirection, deltaTime);
